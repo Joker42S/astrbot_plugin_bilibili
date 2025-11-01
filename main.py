b0b6156b7fd057a8fb7953be029c58f29d1d1aac
Flartiny <1335348298@qq.com>
2025-11-01 10:00:54 +0800

    refactor: get_bangumi

--- main.py
+++ main.py
@@ -12,21 +12,19 @@ from astrbot.api.event import MessageEventResult, AstrMessageEvent, MessageChain
 from astrbot.api.event.filter import (
     command,
     regex,
-    llm_tool,
     permission_type,
     PermissionType,
     event_message_type,
     EventMessageType,
 )
-from bilibili_api import bangumi
-from bilibili_api.bangumi import IndexFilter as IF
 
 from .utils import *
 from .renderer import Renderer
 from .bili_client import BiliClient
 from .listener import DynamicListener
 from .data_manager import DataManager
-from .constant import category_mapping, VALID_FILTER_TYPES, BV, LOGO_PATH
+from .constant import VALID_FILTER_TYPES, BV, LOGO_PATH
+from .tools.bangumi import BangumiTool
 
 
 @register("astrbot_plugin_bilibili", "Soulter", "", "", "")
@@ -49,9 +47,9 @@ class Main(Star):
             data_manager=self.data_manager,
             bili_client=self.bili_client,
             renderer=self.renderer,
-            cfg = self.cfg
+            cfg=self.cfg,
         )
-
+        self.context.add_llm_tools(BangumiTool())
         self.dynamic_listener_task = asyncio.create_task(self.dynamic_listener.start())
 
     @regex(BV)
@@ -139,9 +137,9 @@ class Main(Star):
             avatar = usr_info["face"]
             # 获取最新一条动态 (用于初始化 last_id)
             dyn = await self.bili_client.get_latest_dynamics(int(uid))
-            _, dyn_id = (await self.dynamic_listener._parse_and_filter_dynamics(
-                dyn, _sub_data
-            ))[0]
+            _, dyn_id = (
+                await self.dynamic_listener._parse_and_filter_dynamics(dyn, _sub_data)
+            )[0]
             _sub_data["last"] = dyn_id  # 更新 last id
         except Exception as e:
             logger.error(f"获取 {name} 初始动态失败: {e}")
@@ -224,55 +222,6 @@ class Main(Star):
         else:
             return MessageEventResult().message("未找到指定的订阅")
 
-    @llm_tool("get_bangumi")
-    async def get_bangumi(
-        self,
-        event: AstrMessageEvent,
-        style: str = "ALL",
-        season: str = "ALL",
-        start_year: int = None,
-        end_year: int = None,
-    ):
-        """当用户希望推荐番剧时调用。根据用户的描述获取前 5 条推荐的动漫番剧。
-
-        Args:
-            style(string): 番剧的风格。默认为全部。可选值有：原创, 漫画改, 小说改, 游戏改, 特摄, 布袋戏, 热血, 穿越, 奇幻, 战斗, 搞笑, 日常, 科幻, 萌系, 治愈, 校园, 儿童, 泡面, 恋爱, 少女, 魔法, 冒险, 历史, 架空, 机战, 神魔, 声控, 运动, 励志, 音乐, 推理, 社团, 智斗, 催泪, 美食, 偶像, 乙女, 职场
-            season(string): 番剧的季度。默认为全部。可选值有：WINTER, SPRING, SUMMER, AUTUMN。其也分别代表一月番、四月番、七月番、十月番
-            start_year(number): 起始年份。默认为空，即不限制年份。
-            end_year(number): 结束年份。默认为空，即不限制年份。
-        """
-
-        if style in category_mapping:
-            style = getattr(IF.Style.Anime, category_mapping[style], IF.Style.Anime.ALL)
-        else:
-            style = IF.Style.Anime.ALL
-
-        if season in ["WINTER", "SPRING", "SUMMER", "AUTUMN"]:
-            season = getattr(IF.Season, season, IF.Season.ALL)
-        else:
-            season = IF.Season.ALL
-
-        filters = bangumi.IndexFilterMeta.Anime(
-            area=IF.Area.JAPAN,
-            year=IF.make_time_filter(start=start_year, end=end_year, include_end=True),
-            season=season,
-            style=style,
-        )
-        index = await bangumi.get_index_info(
-            filters=filters, order=IF.Order.SCORE, sort=IF.Sort.DESC, pn=1, ps=5
-        )
-
-        result = "推荐的番剧:\n"
-        for item in index["list"]:
-            result += f"标题: {item['title']}\n"
-            result += f"副标题: {item['subTitle']}\n"
-            result += f"评分: {item['score']}\n"
-            result += f"集数: {item['index_show']}\n"
-            result += f"链接: {item['link']}\n"
-            result += "\n"
-        result += "请分点，贴心地回答。不要输出 markdown 格式。"
-        return result
-
     @permission_type(PermissionType.ADMIN)
     @command("全局删除", alias={"bili_global_del"})
     async def global_sub_del(self, event: AstrMessageEvent, sid: str = None):
@@ -323,9 +272,9 @@ class Main(Star):
                 return MessageEventResult().message(msg)
 
             dyn = await self.bili_client.get_latest_dynamics(int(uid))
-            _, dyn_id = (await self.dynamic_listener._parse_and_filter_dynamics(
-                dyn, _sub_data
-            ))[0]
+            _, dyn_id = (
+                await self.dynamic_listener._parse_and_filter_dynamics(dyn, _sub_data)
+            )[0]
             _sub_data["last"] = dyn_id
         except Exception as e:
             logger.error(f"获取 {usr_info['name']} 初始动态失败: {e}")
